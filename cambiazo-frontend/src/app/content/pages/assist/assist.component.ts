c77210b6ff109658199bc20ed58ae97f663703bc
diegooo01 <u202219639@upc.edu.pe>
2024-05-01 17:33:02 -0500

    Feat: Help first part added
    Added the first part of Help

--- cambiazo-frontend/src/app/content/pages/assist/assist.component.ts
+++ cambiazo-frontend/src/app/content/pages/assist/assist.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-assist',
+  selector: 'app-help',
   standalone: true,
   imports: [],
   templateUrl: './assist.component.html',
