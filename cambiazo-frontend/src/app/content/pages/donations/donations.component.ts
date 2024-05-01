d31248bd949e1039d967e2296c26ca8a86d5a365
123-Mathias <u202216282@upc.edu.pe>
2024-05-01 13:49:38 -0500

    Feat: Components to use placed on the Donations page
    The components that will be used on the Donations page were imported

--- cambiazo-frontend/src/app/content/pages/donations/donations.component.ts
+++ cambiazo-frontend/src/app/content/pages/donations/donations.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {FilterOngsComponent} from "../../components/filter-ongs/filter-ongs.component";
+import {SearchOngsComponent} from "../../components/search-ongs/search-ongs.component";
 
 @Component({
   selector: 'app-donations',
   standalone: true,
-  imports: [],
+  imports: [
+    FilterOngsComponent,
+    SearchOngsComponent
+  ],
   templateUrl: './donations.component.html',
   styleUrl: './donations.component.css'
 })
