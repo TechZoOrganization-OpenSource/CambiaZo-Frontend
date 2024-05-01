166a1e56f87181eb0f2366f8feea9c5650acefcd
123-Mathias <u202216282@upc.edu.pe>
2024-05-01 13:10:17 -0500

    Feat: Product service and search-post-content configuration
    The product service is performed to have the product categories, the search-post-content component was also configured

--- cambiazo-frontend/src/app/content/service/posts/posts.service.ts
+++ cambiazo-frontend/src/app/content/service/posts/posts.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient) {
+  }
+
+
+  getCategoriesProducts():Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/categories-objects`)
+  }
 }
