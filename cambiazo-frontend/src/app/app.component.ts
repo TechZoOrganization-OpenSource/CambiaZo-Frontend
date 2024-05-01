ca58f57f8d010c66bdd96d576ede6a10bb268162
IanHD04 <u202214059@upc.edu.pe>
2024-05-01 11:40:59 -0500

    Feat: Footer Content
    Footer content was added, and the images used were also uploaded

--- cambiazo-frontend/src/app/app.component.ts
+++ cambiazo-frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterContentComponent} from "./public/footer-content/footer-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cambiazo-frontend';
+  title = 'Cambiazo';
 }
