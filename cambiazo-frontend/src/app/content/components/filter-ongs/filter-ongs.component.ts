51cc1a0681e581a27a2e2dff1275d01520b6ec02
123-Mathias <u202216282@upc.edu.pe>
2024-05-01 13:43:04 -0500

    Feat: Creation of the Donations page
    The necessary components, services and models were created

---
+++ cambiazo-frontend/src/app/content/components/filter-ongs/filter-ongs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-ongs',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-ongs.component.html',
+  styleUrl: './filter-ongs.component.css'
+})
+export class FilterOngsComponent {
+
+}
---
+++ cambiazo-frontend/src/app/content/components/search-ongs/search-ongs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-ongs',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-ongs.component.html',
+  styleUrl: './search-ongs.component.css'
+})
+export class SearchOngsComponent {
+
+}
