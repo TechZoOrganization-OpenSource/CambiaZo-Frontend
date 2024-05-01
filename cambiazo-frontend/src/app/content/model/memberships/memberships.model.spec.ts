fba150190880fd8800ddb381c0c1afbe837e1be5
AndresVZ23 <abelandresvallez@gmail.com>
2024-05-01 15:21:58 -0500

    Feat: Creation of the Memberships page
    The necessary components, services and models were created

---
+++ cambiazo-frontend/src/app/content/model/memberships/memberships.model.spec.ts
@@ -0,0 +1,7 @@
+import { Memberships } from './memberships.model';
+
+describe('Memberships', () => {
+  it('should create an instance', () => {
+    expect(new Memberships()).toBeTruthy();
+  });
+});
