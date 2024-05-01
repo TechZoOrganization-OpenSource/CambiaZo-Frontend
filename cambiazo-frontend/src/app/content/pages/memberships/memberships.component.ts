326927ee9714926deef97e155088978533ce8f58
AndresVZ23 <abelandresvallez@gmail.com>
2024-05-01 15:41:55 -0500

    Feat: Memberships service and memberships configuration
    The Memberships service is performed to have the memberships ,the memberships component was also configured

--- cambiazo-frontend/src/app/content/pages/memberships/memberships.component.ts
+++ cambiazo-frontend/src/app/content/pages/memberships/memberships.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {NgClass, NgForOf} from "@angular/common";
+import {MembershipsService} from "../../service/memberships/memberships.service";
+import {Memberships} from "../../model/memberships/memberships.model";
 
 @Component({
   selector: 'app-memberships',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './memberships.component.html',
   styleUrl: './memberships.component.css'
 })
-export class MembershipsComponent {
+export class MembershipsComponent implements OnInit{
 
+  memberships: Memberships[] = []
+
+  constructor(private membershipsService:MembershipsService) {
+  }
+
+
+  ngOnInit() {
+    this.getMemberAllMemberships()
+  }
+
+  getMemberAllMemberships() {
+    this.membershipsService.getMemberShips().subscribe(
+      (res:any)=> {
+        this.memberships = res
+      },error => console.log(error)
+    )
+  }
 }
