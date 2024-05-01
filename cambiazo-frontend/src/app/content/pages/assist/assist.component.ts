4da86e3cf1ded49f8dead534991024f0c58c900c
diegooo01 <u202219639@upc.edu.pe>
2024-05-01 17:27:07 -0500

    Feat: Creation of the help page
    The necessary components were created

---
+++ cambiazo-frontend/src/app/content/pages/assist/assist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assist',
+  standalone: true,
+  imports: [],
+  templateUrl: './assist.component.html',
+  styleUrl: './assist.component.css'
+})
+export class AssistComponent {
+
+}
