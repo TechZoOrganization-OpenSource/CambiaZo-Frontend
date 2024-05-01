a1643a56c270afcb81d3c81d133a27ae96d25f0c
diegooo01 <u202219639@upc.edu.pe>
2024-05-01 12:07:52 -0500

    Feat: Header Content
    Header content was added, and the images used were also uploaded

--- cambiazo-frontend/src/app/app.component.ts
+++ cambiazo-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterContentComponent} from "./public/footer-content/footer-content.component";
+import {HeaderContentComponent} from "./public/header-content/header-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterContentComponent],
+  imports: [RouterOutlet, FooterContentComponent, HeaderContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
