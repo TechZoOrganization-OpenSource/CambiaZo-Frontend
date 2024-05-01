d4dec85ad3c4a12aa1210f45ce2b4d605484fc1d
AndresVZ23 <abelandresvallez@gmail.com>
2024-05-01 16:22:09 -0500

    Feat: Privacy and Policy Page Routing
    The access path for the Privacy and Policy page was created

--- cambiazo-frontend/src/app/app.routes.ts
+++ cambiazo-frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import{DonationsComponent} from "./content/pages/donations/donations.component";
 import {PostComponent} from "./content/pages/post/post.component";
 import {MembershipsComponent} from "./content/pages/memberships/memberships.component";
 import {TermsOfUseComponent} from "./content/pages/terms-of-use/terms-of-use.component";
+import {PrivacyPolicyComponent} from "./content/pages/privacy-policy/privacy-policy.component";
 
 export const routes: Routes = [
   {path:'home',component: HomeComponent},
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {path:'home/post',component:PostComponent},
   {path:'memberships', component:MembershipsComponent},
   {path:'terms-of-use', component: TermsOfUseComponent},
+  {path:'privacy-policy', component: PrivacyPolicyComponent},
   {path:'',pathMatch:'full',redirectTo:'home'},
   {path:'**',pathMatch:'full',redirectTo:'home'}
 ];
