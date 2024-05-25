50a42482ac3dddcfcff2bd8fe78864cfa5b421ae
Abel Andrés Valle Zuta <abelandresvallez@gmail.com>
2024-05-25 12:03:11 -0500

    Feat: Add post cards
    In this part we added post cards

--- src/app/content/components/my-posts/my-posts.component.ts
+++ src/app/content/components/my-posts/my-posts.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-my-posts',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatIconModule
+  ],
   templateUrl: './my-posts.component.html',
   styleUrl: './my-posts.component.css'
 })
