e636de001b98989d8ea3797c82323d7d7cfa1db5
123-Mathias <u202216282@upc.edu.pe>
2024-05-28 00:10:12 -0500

    Feat: Functional search products
    In this part, we add the functionality to search for any product. Also fixed the search on ong.

--- src/app/content/components/lates-posts-content/lates-posts-content.component.ts
+++ src/app/content/components/lates-posts-content/lates-posts-content.component.ts
@@ -5,20 +5,28 @@ import {FeaturePostsContentComponent} from "../feature-posts-content/feature-pos
 import {PostsService} from "../../service/posts/posts.service";
 import {MatIcon} from "@angular/material/icon";
 import {Products} from "../../model/products/products.model";
+import {MatButton} from "@angular/material/button";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {Ongs} from "../../model/ongs/ongs.model";
 @Component({
   selector: 'app-lates-posts-content',
   standalone: true,
-  imports: [
-    MatCardModule,
-    CommonModule,
-    FeaturePostsContentComponent,
-    MatIcon
-  ],
+    imports: [
+        MatCardModule,
+        CommonModule,
+        FeaturePostsContentComponent,
+        MatIcon,
+        MatButton,
+        MatFormField,
+        MatInput
+    ],
   templateUrl: './lates-posts-content.component.html',
   styleUrl: './lates-posts-content.component.css'
 })
 export class LatesPostsContentComponent implements OnInit{
   items:Products[]=[]
+  categories:any[] = []
 
   constructor(private postService:PostsService) {
   }
@@ -45,12 +53,23 @@ export class LatesPostsContentComponent implements OnInit{
       })
 
       this.postService.getCategoriesProducts().subscribe((categories:any)=>{
+        this.categories = categories
         this.items.map((item:Products)=>{
           item.setCategory = categories.find((category:any)=>category.id === item.category_id).name
         })
       })
 
     })
+  }
+
+  searchProduct(event:Event) {
+    const search = (event.target as HTMLInputElement).value.trim()
+    this.postService.getProducs().subscribe((res: any) => {
+      this.items = res.filter((product: Products) => {
+        const ongsNameNormalized = product.product_name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+        return ongsNameNormalized.includes(search) || search == '';
+      })
+    },error => console.log(error))
   }
 
 }
