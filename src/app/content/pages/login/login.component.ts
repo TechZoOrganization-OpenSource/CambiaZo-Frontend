240505a0a379ceacd0778170d99c380ff2c5e064
IanHD04 <u202214059@upc.edu.pe>
2024-05-28 13:04:09 -0500

    Feat: Creation of dialog-password-changed-successfully
    The dialog was created to display the successfully changed password message.

--- src/app/content/pages/login/login.component.ts
+++ src/app/content/pages/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from '@angular/router';
-import {FormsModule} from "@angular/forms";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {FooterContent2Component} from "../../../public/footer-content-2/footer-content-2.component";
 import {UsersService} from "../../service/users/users.service";
 import {Users} from "../../model/users/users.model";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
@@ -20,6 +21,9 @@ import {Users} from "../../model/users/users.model";
     MatLabel,
     RouterLinkActive,
     FooterContent2Component,
+    NgIf,
+    MatSuffix,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -27,13 +31,43 @@ import {Users} from "../../model/users/users.model";
 export class LoginComponent {
   username: string = "";
   password: string = "";
+  showError: boolean = false;
+  errorMessage: string = "";
+  hide = true;
 
-  constructor(private router: Router,private userService: UsersService) { }
+  constructor(private router: Router, private userService: UsersService) { }
 
+  login(): void {
+    this.showError = false;
+    this.userService.login({ username: this.username, password: this.password }).subscribe((result: any) => {
+      if (result === true) {
+        this.router.navigateByUrl('/home');
+      } else if (result === 'password') {
+        this.errorMessage = 'Contraseña incorrecta';
+      } else if (result === 'user') {
+        this.errorMessage = 'Usuario no encontrado';
+      } else {
+        this.errorMessage = 'Error al intentar iniciar sesión';
+      }
+      if (result !== true) {
+        this.showError = true;
+      }
+    });
+  }
+  clearErrorMessage(): void {
+    this.showError = false;
+  }
 
-  login():void{
-    this.userService.login({username:this.username,password:this.password}).subscribe((login:boolean) => {
-      if(login)this.router.navigateByUrl('/home')
-    })
+  onUsernameInput(): void {
+    this.clearErrorMessage();
+  }
+
+  onPasswordInput(): void {
+    this.clearErrorMessage();
+  }
+
+  hide1(event: MouseEvent) {
+    this.hide = !this.hide;
+    event.stopPropagation();
   }
 }
