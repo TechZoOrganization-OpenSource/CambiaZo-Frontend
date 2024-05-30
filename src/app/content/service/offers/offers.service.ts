27dae372258d2374ef72a126cde46209345382ea
diegooo01 <criollodiego542@gmail.com>
2024-05-30 16:15:22 -0500

    Feat: Create offer service
    I've created the offer service

---
+++ src/app/content/service/offers/offers.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Offers } from '../../model/offers/offers.model';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OffersService {
+  baseUrl = `${environment.baseUrl}/offers`;
+
+  constructor(private http: HttpClient) {}
+
+  getOffers(): Observable<Offers[]> {
+    return this.http.get<Offers[]>(this.baseUrl);
+  }
+
+  postOffer(offer: Offers): Observable<Offers> {
+    return this.http.post<Offers>(this.baseUrl, offer);
+  }
+}
