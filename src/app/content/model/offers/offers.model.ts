5a584922cac44a1bba0586cd496609011cc23c97
diegooo01 <criollodiego542@gmail.com>
2024-05-30 16:14:05 -0500

    Feat: Create offers model
    I've created the offer model

---
+++ src/app/content/model/offers/offers.model.ts
@@ -0,0 +1,24 @@
+export class Offers {
+  id_user_offers: string;
+  id_product_offers: string;
+  id_user_get: string;
+  id_product_get: string;
+  status: string;
+  id: string;
+
+  constructor(
+    id_user_offers: string,
+    id_product_offers: string,
+    id_user_get: string,
+    id_product_get: string,
+    status: string,
+    id: string
+  ) {
+    this.id_user_offers = id_user_offers;
+    this.id_product_offers = id_product_offers;
+    this.id_user_get = id_user_get;
+    this.id_product_get = id_product_get;
+    this.status = status;
+    this.id = id;
+  }
+}
