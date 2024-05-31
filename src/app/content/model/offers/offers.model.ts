47d9aab75d13bdb74334af6808544bb3ee72ea44
123-Mathias <u202216282@upc.edu.pe>
2024-05-31 04:10:19 -0500

    Feat: Style Upload Images
    In this part, we added style CSS in the container to upload the product's images.

--- src/app/content/model/offers/offers.model.ts
+++ src/app/content/model/offers/offers.model.ts
@@ -7,12 +7,12 @@ export class Offers {
   id: string;
 
   constructor(
+    id: string,
     id_user_offers: string,
     id_product_offers: string,
     id_user_get: string,
     id_product_get: string,
-    status: string,
-    id: string
+    status: string
   ) {
     this.id_user_offers = id_user_offers;
     this.id_product_offers = id_product_offers;
